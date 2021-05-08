e001345a2ebd73d2c29bb6053a3ec1eb42470399
matin <matin.003mmm@gmail.com>
2021-05-09 00:13:49 +0430

    the constractor fun is ready to merge

--- Circular_linked_list.h
+++ Circular_linked_list.h
@@ -1,20 +1,24 @@
 #include "node.h"
 class Circular_linked_list
 {
-    private:
-        int size;
-        node* current;
-    public:
-        Circular_linked_list();
-        ~Circular_linked_list();
-        //add getter and setter functions here
-        /*
+private:
+    int size;
+    node* current;
+public:
+    Circular_linked_list()
+    {
+        this->current = nullptr ;
+        this->size = 0 ;
+    }
+    ~Circular_linked_list();
+    //add getter and setter functions here
+    /*
             here
         */
-        void pushf();
-        void popf();
-        void pop(int data);
-        void push_after(int new_data,int pa_data);
-        void push_befor(int new_data,int pb_data);
-        void print();
+    void pushf();
+    void popf();
+    void pop(int data);
+    void push_after(int new_data,int pa_data);
+    void push_befor(int new_data,int pb_data);
+    void print();
 };
