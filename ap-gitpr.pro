fd38c2f4c632a3ce32141ea873fa99761afaf851
matin <matin.003mmm@gmail.com>
2021-05-08 13:40:05 +0430

    debug a node class and fix problem

--- ap-gitpr.pro
+++ ap-gitpr.pro
@@ -1,9 +1,10 @@
 QT -= gui
 TARGET = cll
 HEADERS += node.h
+HEADERS += Circular_linked_list.h
 CONFIG += c++11 console
 CONFIG -= app_bundle
-SOURCES += \ cl.cpp
+SOURCES +=  main.cpp
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
--- node.h
+++ node.h
@@ -1,39 +1,40 @@
 //-----------------------------------------------------//writen by matin // https://github.com/mat-mot/ap-git-practice.git //993623038
-class node{
-	private:
-		int data;
-		node* next;
-		node* prev;
-	public:
-        node (int d = 0 , node * n = nullptr , node * p = nullptr )
-        {
-            this->data = d ;
-            this->next = n ;
-            this->prev = p ;
-        }
-        void setdata (int d )
-        {
-            this->data = d ;
-        }
-        int getdata ()
-        {
-            return this->data ;
-        }
-        void setnext (node * n )
-        {
-            this->next = n ;
-        }
-        node * getnext ()
-        {
-            return this->next ;
-        }
-        void setprev (node * p )
-        {
-            this->prev = p ;
-        }
-        node * getprev ()
-        {
-            return this->prev ;
-        }
+class node
+{
+private:
+    int data;
+    node* next;
+    node* prev;
+public:
+    node (int d = 0 , node * n = nullptr , node * p = nullptr )
+    {
+        this->data = d ;
+        this->next = n ;
+        this->prev = p ;
+    }
+    void setdata (int d )
+    {
+        this->data = d ;
+    }
+    int getdata ()
+    {
+        return this->data ;
+    }
+    void setnext (node * n )
+    {
+        this->next = n ;
+    }
+    node * getnext ()
+    {
+        return this->next ;
+    }
+    void setprev (node * p )
+    {
+        this->prev = p ;
+    }
+    node * getprev ()
+    {
+        return this->prev ;
+    }
 };
 //------------------------------------------------------//writen by matin // https://github.com/mat-mot/ap-git-practice.git //993623038
