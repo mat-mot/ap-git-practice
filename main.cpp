ef77595874c4e0810d2168611fb55e918736c055
meh3one <meh3one@gmail.com>
2021-05-12 00:13:47 +0430

    added user interface(????) to main.cpp

--- main.cpp
+++ main.cpp
@@ -17,12 +17,18 @@ void function ()
     int dat1   ;
     int dat2   ;
 
+    ll.pushf(1) ;
+    ll.pushf(2) ;
+    ll.pushf(3) ;
+    ll.pushf(4) ;
+    ll.pushf(5) ;
+    ll.pushf(6) ;
 print_guide() ;
     while(true)
     {
     string order ;
 
-    cout<<"\nwhat shoul i do ?  :  " ;
+    cout<<"what shoul i do ?  :  " ;
     cin>>order ;
 
     if     (order=="pushf")
@@ -33,31 +39,39 @@ print_guide() ;
             }
     else if(order=="popf")
             {
-//            ll.popf() ;
+            ll.popf() ;
             }
     else if(order=="pop")
             {
+            cout<<"which data you want to remove ?  : " ;
             cin>>dat1   ;
-//            ll.pop(dat1) ;
+            ll.pop(dat1) ;
             }
-    else if(order=="push_after")
+    else if(order=="pusha")
             {
+            cout<<"what do you want to add ? : " ;
             cin>>dat1   ;
+            cout<<"after which data you want this to be ? : " ;
             cin>>dat2   ;
-//            ll.push_after(dat1,dat2) ;
+            ll.push_after(dat2,dat1) ;
             }
-    else if(order=="push_before")
+    else if(order=="pushb")
             {
-            cin>>dat1  ;
+            cout<<"what do you want to add ? : " ;
+            cin>>dat1   ;
+            cout<<"before which data you want this to be ? : " ;
             cin>>dat2  ;
-//            ll.push_befor(dat1,dat2) ;
+            ll.push_befor(dat2,dat1) ;
             }
     else if(order=="print")
             {
+            cout<<"datas Are : " ;
             ll.print() ;
+            cout<<endl ;
             }
     else if(order=="finish")
             {
+            cout<<"\nallright it was nice having you\n" ;
             break ;
             }
     else if(order=="clear")
@@ -68,7 +82,7 @@ print_guide() ;
             }
     else
             {
-            cout<<"you did not enter a valid argument try again " ;
+            cout<<"you did not enter a valid argument try again \n" ;
             }
     }
 }
@@ -77,11 +91,12 @@ void print_guide()
 {
 cout<<"|---------Guide---------|\n"
     <<" pushf       :  for adding data to the begining\n"
-    <<" popf        :  for deleting the first data\n"
-    <<" pop         :  for deleting"
-    <<" push_after  :  for adding data next to an existing one \n"
-    <<" push_before :  for adding data before a data \n "
+    <<" popf        :  for deleting the first data \n"
+    <<" pop         :  for deleting data \n"
+    <<" pusha       :  for adding data after  an existing data \n"
+    <<" pushb       :  for adding data before an existing data \n"
     <<" print       :  for printing data \n"
-    <<" finish      :  stopping the program \n\n" ;
+    <<" finish      :  stopping the program \n"
+    <<" clear       :  for clearing the console \n\n" ;
 }
 //--------------https://github.com/mat-mot/ap-git-practice.git ----------------------
